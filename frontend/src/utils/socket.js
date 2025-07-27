f4e80657bff956fa518a36eb6dbb14a51ec61cd6
Sonu Parvez <92024975+CodeBase404@users.noreply.github.com>
2025-07-27 11:05:50 +0530

    Update socket.js

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,7 +1,7 @@
 // utils/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000", {
+export const socket = io(import.meta.env.VITE_BACKEND, {
   withCredentials: true,
   autoConnect: false,
 });
